39001db7972511ee8484877c4a63e3ab8e4e6743
greendrop <greendrop.devel@gmail.com>
2020-09-11 23:43:23 +0900

    yarn add @fortawesome/free-solid-svg-icons

--- src/components/organisms/DrawerContent.vue
+++ src/components/organisms/DrawerContent.vue
@@ -1,5 +1,17 @@
 <template>
-  <div />
+  <div>
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title class="title">
+          Application
+        </v-list-item-title>
+        <v-list-item-subtitle>
+          subtext
+        </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+    <v-divider />
+  </div>
 </template>
 
 <script lang="ts">
